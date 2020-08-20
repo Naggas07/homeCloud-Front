b87fa23131c461184af664309a487ee84324b953
Naggas07 <rober.araya.gil@gmail.com>
2020-08-20 18:32:59 +0200

    first look of login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,6 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "../styles/css/login.css";
+import logo from "../styles/images/cloud-icon.png";
 
 class Login extends Component {
   constructor(props) {
@@ -7,7 +8,41 @@ class Login extends Component {
     this.state = {};
   }
   render() {
-    return <div className="loginBack">hola</div>;
+    return (
+      <div className="loginBack">
+        <div className="login-container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <h3 className="tittle-text">myCloud</h3>
+          </div>
+          <div className="form login-font">
+            <form className="form-margins">
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  autoComplete="off"
+                  placeholder="email"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  autoComplete="off"
+                  placeholder="Contraseña"
+                />
+              </div>
+              <button type="submit" className="btn btn-block primary-button">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
