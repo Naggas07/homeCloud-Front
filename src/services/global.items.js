a87871d26dd2619066dae5818398e857b2368b4a
Naggas07 <rober.araya.gil@gmail.com>
2020-08-24 10:34:22 +0200

    change API & css

--- src/services/global.items.js
+++ src/services/global.items.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000"; //process.env.REACT_APP_API || "http://localhost:5000";
+const API = process.env.REACT_APP_API || "http://localhost:5000";
 
 console.log(API);
 
